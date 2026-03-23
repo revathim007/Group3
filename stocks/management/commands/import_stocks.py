6958ee3c20775ca97fb70e253374f46415174cb3
revathim007 <reha070302@gmail.com>
2026-03-23 14:33:14 +0530

    feat: Implement error boundary and update stock categorization

--- stocks/management/commands/import_stocks.py
+++ stocks/management/commands/import_stocks.py
@@ -6,6 +6,24 @@ from stocks.models import Stock
 class Command(BaseCommand):
     help = 'Import stocks from an Excel file'
 
+    # Nifty indices and their corresponding sectors
+    NIFTY_SECTORS = {
+        'Nifty Auto': ['ASHOKLEY', 'BAJAJ-AUTO', 'BHARATFORG', 'BOSCHLTD', 'EICHERMOT', 'HEROMOTOCO', 'M&M', 'MARUTI', 'MRF', 'SAMVARDHANA', 'TATAMOTORS', 'TVSMOTOR', 'TIINDIA', 'BALKRISIND', 'EXIDEIND'],
+        'Nifty Bank': ['AUBANK', 'AXISBANK', 'BANDHANBNK', 'BANKBARODA', 'FEDERALBNK', 'HDFCBANK', 'ICICIBANK', 'IDFCFIRSTB', 'INDUSINDBK', 'KOTAKBANK', 'PNB', 'SBIN'],
+        'Nifty Commodities': ['ADANIENT', 'ADANIPORTS', 'AMBUJACEM', 'ASIANPAINT', 'BAJAJ-AUTO', 'BHARATFORG', 'BPCL', 'COALINDIA', 'EICHERMOT', 'GRASIM', 'HINDALCO', 'HINDUNILVR', 'IOC', 'JSWSTEEL', 'M&M', 'MARUTI', 'NESTLEIND', 'NTPC', 'ONGC', 'POWERGRID', 'RELIANCE', 'SHREECEM', 'SUNPHARMA', 'TATACONSUM', 'TATAMOTORS', 'TATASTEEL', 'TECHM', 'TITAN', 'ULTRACEMCO'],
+        'Nifty CPSE': ['BEL', 'BHEL', 'COALINDIA', 'GAIL', 'HAL', 'IOC', 'NMDC', 'NTPC', 'ONGC', 'POWERGRID', 'SAIL', 'RECLTD', 'PFC'],
+        'Nifty Energy': ['ADANIENT', 'BPCL', 'COALINDIA', 'GAIL', 'HINDPETRO', 'IOC', 'NTPC', 'ONGC', 'POWERGRID', 'RELIANCE', 'TATAPOWER'],
+        'Nifty FMCG': ['BRITANNIA', 'COLPAL', 'DABUR', 'GODREJCP', 'HINDUNILVR', 'ITC', 'MARICO', 'NESTLEIND', 'PGHH', 'TATACONSUM', 'UBL', 'VBL', 'EMAMILTD', 'RADICO', 'JUBLFOOD'],
+        'Nifty IT': ['COFORGE', 'HCLTECH', 'INFY', 'LTIM', 'Mphasis', 'PERSISTENT', 'TCS', 'TECHM', 'WIPRO', 'LTI'],
+        'Nifty Media': ['DBCORP', 'JAGRAN', 'NDTV', 'SUNTV', 'TV18BRDCST', 'TVTODAY', 'ZEEL', 'PVRINOX', 'SAREGAMA', 'NETWORK18'],
+        'Nifty Metal': ['ADANIENT', 'APLAPOLLO', 'HINDALCO', 'HINDZINC', 'JINDALSTEL', 'JSWSTEEL', 'NMDC', 'SAIL', 'TATASTEEL', 'VEDL', 'WELCORP', 'RATNAMANI'],
+        'Nifty MNC': ['3MINDIA', 'ABB', 'ACC', 'AMBUJACEM', 'AKZONOBEL', 'BATAINDIA', 'BAYERCROP', 'BOSCHLTD', 'BRITANNIA', 'CASTROLIND', 'COCA-COLA', 'COLPAL', 'CUMMINSIND', 'GILLETTE', 'GLAXO', 'HINDUNILVR', 'HONEYWELL', 'NESTLEIND', 'PFIZER', 'PGHH', 'SANOFI', 'SIEMENS', 'SKFINDIA', 'TIMKEN', 'WHIRLPOOL'],
+        'Nifty Pharma': ['ALKEM', 'AUROPHARMA', 'BIOCON', 'CIPLA', 'DIVISLAB', 'DRREDDY', 'GLENMARK', 'GRANULES', 'IPCALAB', 'LAURUSLABS', 'LUPIN', 'SUNPHARMA', 'TORNTPHARM', 'Zyduslife', 'GLAND'],
+        'Nifty PSE': ['BEL', 'BHEL', 'COALINDIA', 'GAIL', 'HAL', 'HINDPETRO', 'IOC', 'NMDC', 'NTPC', 'ONGC', 'POWERGRID', 'SAIL', 'RECLTD', 'PFC', 'BANKBARODA', 'CANBK', 'UNIONBANK', 'SBIN'],
+        'Nifty PSU Bank': ['BANKBARODA', 'BANKINDIA', 'CANBK', 'CENTRALBK', 'INDIANB', 'IOB', 'MAHABANK', 'PNB', 'PSB', 'SBIN', 'UCOBANK', 'UNIONBANK'],
+        'Nifty Realty': ['BRIGADE', 'DLF', 'GODREJPROP', 'IBREALEST', 'LODHA', 'MAHSEAMLES', 'OBEROIRLTY', 'PHOENIXLTD', 'PRESTIGE', 'SOBHA'],
+    }
+
     def handle(self, *args, **options):
         file_path = 'd:\\Revathi\\Biz Metric Internship\\Project 2026\\revathiMaithiliproject\\StockVerse_Stoxie\\USA Top 200 Stocks.xlsx'
         try:
@@ -16,7 +34,15 @@ class Command(BaseCommand):
                 
                 stock.name = row['Company']
                 stock.exchange = 'NASDAQ'  # Assuming NASDAQ for US stocks
-                stock.sector = row.get('Sector', None)
+
+                # Find the sector for the stock
+                for sector, symbols in self.NIFTY_SECTORS.items():
+                    if stock_symbol in symbols:
+                        stock.sector = sector
+                        break
+                else:
+                    stock.sector = row.get('Sector', None)
+
                 stock.current_price = row.get('Price', None)
                 stock.currency = 'USD'
                 stock.pe_ratio = row.get('PE Ratio', None)
